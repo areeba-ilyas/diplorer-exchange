f1ab244df14b176339846f2265e083b1d1fc70e0
areeba ilyas <areebakhan2k22@gmail.com>
2025-10-05 10:28:20 +0500

    fix: fresh orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,8 +1,1 @@
-﻿export default function OrdersPage() {
-  return (
-    <div>
-      <h1>Orders Page</h1>
-      <p>Your orders will appear here.</p>
-    </div>
-  );
-}
+export default function OrdersPage() { return <div>Orders</div>; }
