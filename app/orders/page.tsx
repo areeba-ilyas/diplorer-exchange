cf203dc2f6e8e16b7bd96f1a0efb7097b6f9f0c4
areeba ilyas <areebakhan2k22@gmail.com>
2025-10-05 10:17:50 +0500

    fix: completely rebuild orders folder and remove corrupted files

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,8 +1,8 @@
-export default function OrdersPage() {
+﻿export default function OrdersPage() {
   return (
     <div>
       <h1>Orders Page</h1>
-      <p>Orders will be displayed here.</p>
+      <p>Your orders will appear here.</p>
     </div>
   );
 }
